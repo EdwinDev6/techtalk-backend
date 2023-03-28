0d7c4bfeee4b1ebe78e651d19028972965e8dbdf
edwin6666 <edwinmendoza0620@gmail.com>
2023-03-28 13:39:11 -0400

    Feat: auth module import

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,10 +1,10 @@
-import { Router } from "express";
-import { createUser } from "../controllers/user.controller.js";
-import { checkExistingUser } from "../middlewares/verifySignup.js";
-const Token = require('../middlewares/authJwt')
+import { Router } from 'express';
+import { createUser } from '../controllers/user.controller.js';
+import { checkExistingUser } from '../middlewares/verifySignup.js';
+import { verifyToken, isAdmin } from '../middlewares/authJwt.js';
 
 const router = Router();
 
-router.post("/", Token.verifyToken,Token.isAdmin, checkExistingUser, createUser);
+router.post('/', verifyToken, isAdmin, checkExistingUser, createUser);
 
 export default router;
