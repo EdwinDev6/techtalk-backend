7d0694cf6f873457f04e749fdd33fe3fec152c32
edwin6666 <edwinmendoza0620@gmail.com>
2023-10-17 23:40:46 -0400

    Feat(roles): feat role moderator

--- backend/src/routes/post.routes.js
+++ backend/src/routes/post.routes.js
@@ -1,18 +1,18 @@
 import { Router } from 'express';
 import {getPost, getPosts ,createPost, updatePost, removePost} from '../controllers/post.controller.js';
-import { verifyToken, isAdmin } from '../middlewares/authJwt.js';
+import { verifyToken,  isModerator} from '../middlewares/authJwt.js';
 
 const router = Router();
 
 
-router.post('/',verifyToken, isAdmin,  createPost);
+router.post('/',verifyToken, isModerator,  createPost);
 
 router.get('/', getPosts);
 
 router.get('/:postId', getPost);
 
-router.put('/:postId',verifyToken, isAdmin, updatePost);
+router.put('/:postId',verifyToken, isModerator, updatePost);
 
-router.delete('/:postId',verifyToken, isAdmin, removePost);
+router.delete('/:postId',verifyToken, isModerator, removePost);
 
 export default router;
