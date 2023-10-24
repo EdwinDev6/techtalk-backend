108a91ed8888d8c89844306f12d8e93e6857102b
edwin6666 <edwinmendoza0620@gmail.com>
2023-10-24 15:25:36 -0400

    Delete consoles

--- backend/src/controllers/subscription.controller.js
+++ backend/src/controllers/subscription.controller.js
@@ -20,28 +20,25 @@ export const activateSubscription = async (req, res) => {
       .status(200)
       .json({ message: "Subscription activated successfully.." });
   } catch (error) {
-    console.error("Error activating subscription::", error);
     return res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
-
 export const unsubscribe = async (req, res) => {
-    try {
-      const { email } = req.body;
-  
-      const user = await User.findOne({ email });
-  
-      if (!user) {
-        return res.status(404).json({ message: "User not Found." });
-      }
-  
-      user.subscription = false;
-      await user.save();
-  
-      return res.status(200).json({ message: "Successful unsubscription." });
-    } catch (error) {
-      console.error("Error unsubscribing:", error);
-      return res.status(500).json({ error: "Internal Server Error" });
+  try {
+    const { email } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not Found." });
     }
-  };
+
+    user.subscription = false;
+    await user.save();
+
+    return res.status(200).json({ message: "Successful unsubscription." });
+  } catch (error) {
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
