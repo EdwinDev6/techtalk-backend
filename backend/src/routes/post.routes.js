4a7f65fe4e88ec482ab36f98ed5007c5048821a4
edwin6666 <edwinmendoza0620@gmail.com>
2023-10-24 14:07:45 -0400

    Feat(comments): comments functionality details: -username is saved in cookies -anyone can comment on any post -the username becomes automatic, preventing people from entering another name

--- backend/src/routes/post.routes.js
+++ backend/src/routes/post.routes.js
@@ -1,18 +1,26 @@
-import { Router } from 'express';
-import {getPost, getPosts ,createPost, updatePost, removePost} from '../controllers/post.controller.js';
-import { verifyToken, isAdmin } from '../middlewares/authJwt.js';
+import { Router } from "express";
+import {
+  getPost,
+  getPosts,
+  createPost,
+  updatePost,
+  removePost,
+  createComment,
+} from "../controllers/post.controller.js";
+import { verifyToken, isAdmin } from "../middlewares/authJwt.js";
 
 const router = Router();
 
+router.post("/", verifyToken, isAdmin, createPost);
 
-router.post('/',verifyToken, isAdmin,  createPost);
+router.get("/", getPosts);
 
-router.get('/', getPosts);
+router.get("/:postId", getPost);
 
-router.get('/:postId', getPost);
+router.put("/:postId", verifyToken, isAdmin, updatePost);
 
-router.put('/:postId',verifyToken, isAdmin, updatePost);
+router.delete("/:postId", verifyToken, isAdmin, removePost);
 
-router.delete('/:postId',verifyToken, isAdmin, removePost);
+router.post("/:postId/comments", verifyToken, createComment);
 
 export default router;
