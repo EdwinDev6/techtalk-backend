883f1282360ff150fd560274974a268ce946d182
edwin6666 <edwinmendoza0620@gmail.com>
2023-10-25 12:38:51 -0400

    Fix: bug in routes duplicate

--- backend/src/routes/post.routes.js
+++ backend/src/routes/post.routes.js
@@ -1,31 +1,26 @@
-
-import { Router } from 'express';
-import {getPost, getPosts ,createPost, updatePost, removePost,createComment} from '../controllers/post.controller.js';
-import { verifyToken,  isModerator} from '../middlewares/authJwt.js';
-
+import { Router } from "express";
+import {
+  getPost,
+  getPosts,
+  createPost,
+  updatePost,
+  removePost,
+  createComment,
+} from "../controllers/post.controller.js";
+import { verifyToken, isModerator } from "../middlewares/authJwt.js";
 
 const router = Router();
 
-router.post("/", verifyToken, isAdmin, createPost);
-
-
 router.get("/", getPosts);
 
-router.post('/',verifyToken, isModerator,  createPost);
-
+router.post("/", verifyToken, isModerator, createPost);
 
 router.get("/:postId", getPost);
-
-router.put("/:postId", verifyToken, isAdmin, updatePost);
-
 
 router.delete("/:postId", verifyToken, isModerator, removePost);
 
 router.post("/:postId/comments", verifyToken, createComment);
 
-router.put('/:postId',verifyToken, isModerator, updatePost);
-
-router.delete('/:postId',verifyToken, isModerator, removePost);
-
+router.put("/:postId", verifyToken, isModerator, updatePost);
 
 export default router;
