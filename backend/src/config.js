6058d14cea642e0a04afd4210970623a782814e2
edwin6666 <edwinmendoza0620@gmail.com>
2024-01-25 22:04:22 -0400

    Feat: Port to deploy

--- backend/src/config.js
+++ backend/src/config.js
@@ -3,7 +3,7 @@ import { resolve } from "path";
 config({ path: resolve(__dirname, "..", ".env") });
 
 export const MONGODB_URI = process.env.MONGODB_URI;
-export const PORT = process.env.PORT;
+export const PORT = process.env.PORT || 4000;
 export const SECRET = process.env.SECRET;
 export const ADMIN_EMAIL = process.env.ADMIN_EMAIL;
 export const ADMIN_USERNAME = process.env.ADMIN_USERNAME;
